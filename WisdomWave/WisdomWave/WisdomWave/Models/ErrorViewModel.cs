7d97a9006981d8c0d5115b2ed57883cad8951815
Mona Lisa <adrozd556@gmail.com>
2023-08-31 21:40:49 +0200

    starting position

---
+++ WisdomWave/WisdomWave/WisdomWave/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WisdomWave.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
