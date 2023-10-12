04191b9d484d95689fb6c3546864e0e3be0a8a34
Kirill <kirillpugach2@gmail.com>
2023-10-13 02:35:30 +0300

    Controller(Courses,User)
    Controller(Courses,User)

---
+++ WisdomWave/BLL/Services/CourseService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Repositories.UnitsOfWork;
+using Domain.Models;
+
+namespace BLL.Services
+{
+    public class CourseService
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public CourseService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<IReadOnlyCollection<Course>> GetAsyncs() => await unitOfWork.CourseRepository.GetAllAsync();
+        public async Task<IReadOnlyCollection<Course>> FindByConditionAsync(Expression<Func<Course, bool>> predicat) => await this.unitOfWork.CourseRepository.FindByConditionAsync(predicat);
+        public async Task<Course> FindByConditionItemAsync(Expression<Func<Course, bool>> predicat) => await this.unitOfWork.CourseRepository.FindByConditionItemAsync(predicat);
+        public async Task CreateAsync(Course course) => await unitOfWork.CourseRepository.CreateAsync(course);
+        public async Task DeleteAsync(int id) => await unitOfWork.CourseRepository.Delete(id);
+        public async Task EditAsync(int id, Course course) => await unitOfWork.CourseRepository.Update(course, id);
+        public async Task<IReadOnlyCollection<Course>> FindAllLearningCoursesForUser(string userId)
+        {
+            List<LearnerUserToCourse> allCourses = (await unitOfWork.LearnerUserToCourseRepository.FindByConditionAsync(x => x.userId == userId)).ToList();
+            List<Course> courses = new List<Course>();
+
+            for (int i = 0; i < allCourses.Count; i++)
+            {
+                LearnerUserToCourse thisC = allCourses.First();
+                courses.Add(await unitOfWork.CourseRepository.FindByConditionItemAsync(c => c.Id == thisC.courseId));
+                allCourses.Remove(thisC);
+            }
+
+            return courses;
+        }
+    }
+}
