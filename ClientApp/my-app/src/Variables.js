c76f66f83a9422f211a99e1f700c2b13f4c94fda
DddrozddD <138630707+DddrozddD@users.noreply.github.com>
2023-12-17 10:33:56 +0100

    Update Variables.js

--- ClientApp/my-app/src/Variables.js
+++ ClientApp/my-app/src/Variables.js
@@ -1,4 +1,4 @@
 export const variables={
-    API_URL:"https://wisdomwave20231217102514.azurewebsites.net",
+    API_URL:"https://wisdomwave20231217102514.azurewebsites.net/api/",
     PAGE_URL:"http://localhost:3000/"
 }
